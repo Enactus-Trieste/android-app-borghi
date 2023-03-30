384b47b979a6caa65757f8e5aec105072f35bafc
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-03-30 19:04:12 +0200

    Added skeleton MainActivity

--- app/src/main/java/it/units/borghisegreti/activities/MainActivity.java
+++ app/src/main/java/it/units/borghisegreti/activities/MainActivity.java
@@ -1,16 +1,36 @@
 package it.units.borghisegreti.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import java.util.Objects;
 
 import it.units.borghisegreti.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostFragment hostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = Objects.requireNonNull(hostFragment, "Host fragment view not found").getNavController();
+
+        navController.addOnDestinationChangedListener(((controller, destination, arguments) -> {
+            // here we can check the destination id and handle changes accordingly
+        }));
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        // needed in case a NavigationDrawer with an AppBarConfiguration is added
+        return super.onSupportNavigateUp();
     }
 }
