20b481599472286f7039eb17c0525164388745c0
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-04-06 14:41:01 +0200

    Refactored Zone to better match database structure

--- app/src/main/java/it/units/borghisegreti/models/Zone.java
+++ app/src/main/java/it/units/borghisegreti/models/Zone.java
@@ -1,22 +1,41 @@
 package it.units.borghisegreti.models;
 
 
+import androidx.annotation.NonNull;
+
 import com.google.android.gms.maps.model.LatLng;
+
+import org.jetbrains.annotations.Contract;
 
 public class Zone {
     private String name;
-    private LatLng coordinates;
+    private double latitude;
+    private double longitude;
 
-    public Zone(String name, LatLng coordinates) {
+    public Zone(@NonNull String name, @NonNull LatLng coordinates) {
         this.name = name;
-        this.coordinates = coordinates;
+        this.latitude = coordinates.latitude;
+        this.longitude = coordinates.longitude;
+    }
+
+    // required for Firebase's getValue(Zone.class)
+    public Zone() {
+
     }
 
     public String getName() {
         return name;
     }
 
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
     public LatLng getCoordinates() {
-        return coordinates;
+        return new LatLng(latitude, longitude);
     }
 }
