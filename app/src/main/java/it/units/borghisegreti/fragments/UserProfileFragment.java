79ab8aaffde9ef0d5844492086fdab19f45c51f7
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-05-14 15:36:46 +0200

    A first user profile layout

--- app/src/main/java/it/units/borghisegreti/fragments/UserProfileFragment.java
+++ app/src/main/java/it/units/borghisegreti/fragments/UserProfileFragment.java
@@ -1,16 +1,28 @@
 package it.units.borghisegreti.fragments;
 
+import static it.units.borghisegreti.utils.Database.DB_URL;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 import it.units.borghisegreti.R;
+import it.units.borghisegreti.databinding.FragmentUserProfileBinding;
+import it.units.borghisegreti.viewmodels.UserProfileViewModel;
 
 public class UserProfileFragment extends Fragment {
+
+    private FragmentUserProfileBinding viewBinding;
+    private UserProfileViewModel viewModel;
 
     public UserProfileFragment() {
         // Required empty public constructor
@@ -20,13 +32,13 @@ public class UserProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        viewModel = new ViewModelProvider(this, new UserProfileViewModel.Factory(FirebaseDatabase.getInstance(DB_URL))).get(UserProfileViewModel.class);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+        viewBinding = FragmentUserProfileBinding.inflate(inflater, container, false);
+        return viewBinding.getRoot();
     }
 }
