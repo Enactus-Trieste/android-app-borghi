800ad7c5c146cefd290b449f76925e79f492d685
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-05-17 16:40:39 +0200

    Added layout with carousel for experience images

--- app/src/main/java/it/units/borghisegreti/fragments/dialogs/ExperienceDialogFragment.java
+++ app/src/main/java/it/units/borghisegreti/fragments/dialogs/ExperienceDialogFragment.java
@@ -1,14 +1,19 @@
 package it.units.borghisegreti.fragments.dialogs;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.carousel.CarouselLayoutManager;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import it.units.borghisegreti.R;
 import it.units.borghisegreti.databinding.FragmentExperienceDialogBinding;
@@ -40,10 +45,19 @@ public class ExperienceDialogFragment extends DialogFragment {
         }
     }
 
+    // called to get the layout, regardless if it's displayed as a dialog or an embedded fragment
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         viewBinding = FragmentExperienceDialogBinding.inflate(inflater, container, false);
         return viewBinding.getRoot();
+    }
+
+    // called only when creating the layout in a dialog
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        return dialog;
     }
 }
