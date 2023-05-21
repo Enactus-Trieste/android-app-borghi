1fab22eeb25a120886712a350afda5bbffaf9887
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-05-21 13:21:55 +0200

    Added placeholder images

--- app/src/main/java/it/units/borghisegreti/adapters/CarouselItem.java
+++ app/src/main/java/it/units/borghisegreti/adapters/CarouselItem.java
@@ -1,19 +1,40 @@
 package it.units.borghisegreti.adapters;
 
-import androidx.annotation.DrawableRes;
+import static it.units.borghisegreti.adapters.CarouselAdapter.CAROUSEL_TAG;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Objects;
 
-public class CarouselItem {
-    @DrawableRes
-    private final int drawableResource;
+import it.units.borghisegreti.R;
 
-    public CarouselItem(@DrawableRes int drawableResource) {
-        this.drawableResource = drawableResource;
+public class CarouselItem {
+    @NonNull
+    private Bitmap image;
+
+    public CarouselItem(@NonNull Context context, @NonNull String imageFileName) {
+        AssetManager assetManager = context.getAssets();
+        try (InputStream is = assetManager.open(imageFileName)) {
+            image = BitmapFactory.decodeStream(is);
+        } catch (IOException e) {
+            image = BitmapFactory.decodeResource(Resources.getSystem(), R.drawable.ic_baseline_image_not_supported_24);
+            Log.e(CAROUSEL_TAG, "Unable to decode carousel image", e);
+        }
     }
 
-    public int getDrawableResource() {
-        return drawableResource;
+    @NonNull
+    public Bitmap getImage() {
+        return image;
     }
 
     @Override
@@ -21,11 +42,11 @@ public class CarouselItem {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         CarouselItem that = (CarouselItem) o;
-        return drawableResource == that.drawableResource;
+        return image.equals(that.image);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(drawableResource);
+        return Objects.hash(image);
     }
 }
--- app/src/main/java/it/units/borghisegreti/fragments/dialogs/ExperienceDialog.java
+++ app/src/main/java/it/units/borghisegreti/fragments/dialogs/ExperienceDialog.java
@@ -1,6 +1,7 @@
 package it.units.borghisegreti.fragments.dialogs;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.view.LayoutInflater;
 
 import androidx.annotation.NonNull;
@@ -47,14 +48,11 @@ public class ExperienceDialog {
     }
 
     @NonNull
-    private static List<CarouselItem> createItems() {
+    private List<CarouselItem> createItems() {
         return Arrays.asList(
-                new CarouselItem(R.drawable.ic_baseline_image_not_supported_24),
-                new CarouselItem(R.drawable.ic_baseline_image_not_supported_24),
-                new CarouselItem(R.drawable.ic_baseline_image_not_supported_24),
-                new CarouselItem(R.drawable.ic_baseline_image_not_supported_24),
-                new CarouselItem(R.drawable.ic_baseline_image_not_supported_24),
-                new CarouselItem(R.drawable.ic_baseline_image_not_supported_24)
+                new CarouselItem(dialog.getContext(), "images/nature_alps.jpg"),
+                new CarouselItem(dialog.getContext(), "images/restaurant_table.jpg"),
+                new CarouselItem(dialog.getContext(), "images/nature_horse.jpg")
         );
     }
 }
