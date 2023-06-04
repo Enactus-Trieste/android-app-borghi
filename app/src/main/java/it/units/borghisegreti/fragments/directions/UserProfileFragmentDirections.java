5373f54e637adfe2e0909e2e4f1dfc461325821c
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-06-04 18:33:06 +0200

    Added user profile fragment navigation

---
+++ app/src/main/java/it/units/borghisegreti/fragments/directions/UserProfileFragmentDirections.java
@@ -0,0 +1,29 @@
+package it.units.borghisegreti.fragments.directions;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.ActionOnlyNavDirections;
+import androidx.navigation.NavDirections;
+
+import org.jetbrains.annotations.Contract;
+
+import it.units.borghisegreti.R;
+
+public class UserProfileFragmentDirections {
+    @NonNull
+    @Contract(" -> new")
+    public static NavDirections actionUserProfileFragmentToMapsFragment() {
+        return new ActionOnlyNavDirections(R.id.action_userProfileFragment_to_mapsFragment);
+    }
+
+    @NonNull
+    @Contract(" -> new")
+    public static NavDirections actionUserProfileFragmentToExperiencesFragment() {
+        return new ActionOnlyNavDirections(R.id.action_userProfileFragment_to_experiencesFragment);
+    }
+
+    @NonNull
+    @Contract(" -> new")
+    public static NavDirections actionUserProfileFragmentToLoginFragment() {
+        return new ActionOnlyNavDirections(R.id.action_userProfileFragment_to_loginFragment);
+    }
+}
