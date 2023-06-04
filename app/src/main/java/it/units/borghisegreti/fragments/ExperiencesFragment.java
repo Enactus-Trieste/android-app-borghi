e9ee45f70f4637ac8dbd59bc8d8256e449262513
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-06-04 20:09:03 +0200

    Added filter chips

--- app/src/main/java/it/units/borghisegreti/fragments/ExperiencesFragment.java
+++ app/src/main/java/it/units/borghisegreti/fragments/ExperiencesFragment.java
@@ -3,15 +3,14 @@ package it.units.borghisegreti.fragments;
 import static it.units.borghisegreti.utils.Database.DB_URL;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.google.android.material.divider.MaterialDividerItemDecoration;
 import com.google.firebase.database.FirebaseDatabase;
