2562c50eb0368f5ededd55286a3dbaab35f2f7b4
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-07-17 18:29:54 +0200

    Refactor

--- app/src/androidTest/java/it/units/borghisegreti/LoginFragmentValidationTests.java
+++ app/src/androidTest/java/it/units/borghisegreti/LoginFragmentValidationTests.java
@@ -4,6 +4,7 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static it.units.borghisegreti.CustomMatchers.hasTextInputLayoutErrorText;
 
 import android.os.Bundle;
 
@@ -13,8 +14,6 @@ import androidx.test.filters.LargeTest;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import static it.units.borghisegreti.CustomMatchers.hasTextInputLayoutErrorText;
 
 import it.units.borghisegreti.fragments.LoginFragment;
 
